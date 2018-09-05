b19ace3c8e5dbb698bc4391e60ab18da21e69d0d
lu.huang@biz-united.com.cn <>
2018-09-05 15:11:34 +0800

    views

--- WDL_TYR/Pages/Delivery/Views/DeliveryPlaceChooiceView.swift
+++ WDL_TYR/Pages/Delivery/Views/DeliveryPlaceChooiceView.swift
@@ -7,15 +7,55 @@
 //
 
 import UIKit
+import RxSwift
 
 class DeliveryPlaceChooiceView: UIView {
+    
+    typealias InputSureClosure = () -> ()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var sureButton: UIButton!
+    @IBOutlet weak var contentView: UIView!
+    
+    private var targetTextField:UITextField?
+    
+    private var closure: InputSureClosure?
+    
+    private var contentInputView:UIView?
+    
+    func insertContentView(inputView:UIView , targetTextField:UITextField) {
+        self.contentInputView = inputView
+        self.targetTextField = targetTextField
     }
-    */
-
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let _ = self.cancelButton.rx.tap.asObservable()
+            .takeUntil(self.rx.deallocated)
+            .subscribe(onNext: { () in
+                self.resignTextFieldResponser()
+            })
+        
+        let _ = self.sureButton.rx.tap.asObservable()
+            .takeUntil(self.rx.deallocated)
+            .subscribe { () in
+                if let closure = self.closure {
+                    closure()
+                }
+                self.resignTextFieldResponser()
+            }
+        }
+    
+    func resignTextFieldResponser() {
+        if let target = self.targetTextField {
+            target.resignFirstResponder()
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.contentInputView?.frame = self.contentView.bounds
+        self.contentView.addSubview(self.contentInputView!)
+    }
 }
---
+++ WDL_TYR/Pages/Delivery/Views/PlaceChooiceView.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceChooiceView.swift
+//  WDL_TYR
+//
+//  Created by 黄露 on 2018/9/5.
+//  Copyright © 2018年 yingli. All rights reserved.
+//
+
+import UIKit
+
+class PlaceChooiceView: UIView {
+    
+//    private lazy var collectionView:UICollectionView = {
+//       
+//    }()
+    
+}
