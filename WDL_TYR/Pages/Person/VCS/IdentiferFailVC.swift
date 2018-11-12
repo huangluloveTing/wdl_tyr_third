79ec9ab5dea1b4a638adfa3e09b100c25c528f89
yaya7305 <13980765103@163.com>
2018-11-12 13:15:55 +0800

    添加

---
+++ WDL_TYR/Pages/Person/VCS/IdentiferFailVC.swift
@@ -0,0 +1,30 @@
+//
+//  IdentiferFailVC.swift
+//  WDL_TYR
+//
+//  Created by Apple on 2018/11/12.
+//  Copyright © 2018 yinli. All rights reserved.
+//
+
+import UIKit
+
+class IdentiferFailVC: NormalBaseVC {
+    //失败原因
+    @IBOutlet weak var failResonLable: UILabel!
+    //重新提交按钮
+    @IBOutlet weak var reSuButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        reSuButton.layer.cornerRadius = 4
+        reSuButton.clipsToBounds = true
+    }
+
+    @IBAction func callClick(_ sender: UIButton) {
+        
+        Util.toCallPhone(num: KF_PHONE_NUM)
+    }
+    //重新提交
+    @IBAction func reSummitClick(_ sender: UIButton) {
+    }
+}
