8beb9c9d8f2eca97f682cd1e7cdd50de2316bd34
yaya7305 <13980765103@163.com>
2018-12-12 14:08:41 +0800

    须知内容添加

--- WDL_TYR/Configs/const.swift
+++ WDL_TYR/Configs/const.swift
@@ -23,8 +23,8 @@ let PUSH_MESSAGE_VALUE = "PUSH_MESSAGE_VALUE"
 
 let GAODE_MAP_KEY = "8e99eeada50ef322b5c993eb92acffd6"
 
-let RE_SHELVE_AGREEN_TITLE : String? = nil        // 确认签收收货须知 标题
-let RE_SHELVE_AGREEN_CONTENT : String? = nil        // 确认签收收货须知 内容
+let RE_SHELVE_AGREEN_TITLE : String = "收货须知"        // 确认签收收货须知 标题
+let RE_SHELVE_AGREEN_CONTENT : String = "1、当您收到货物时，请与送货人员当场验收。\n2、 确认您的商品品牌、名称、数量等产品信息与订单相符。\n3、 包装完好、无雨淋、异味、车厢污染等因在途造成的质量问题。\n4、 确认签收后，即表示您已认可此次交易行为，不再支持因运输产生的其他问题。\n5、 签收成功后如遇到产品自身的质量问题，因联系商品的销售部或客服部门处理。\n"        // 确认签收收货须知 内容
 
 //极光推送的key
 let JPushAppKey = "20aeb9ef1cdc4a5185da6bb6"
