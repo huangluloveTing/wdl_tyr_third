7858368d4966c1a6323e11c03f59854bdaca3f5a
yaya7305 <13980765103@163.com>
2019-02-11 09:43:28 +0800

    update

--- WDL_TYR/Configs/const.swift
+++ WDL_TYR/Configs/const.swift
@@ -31,7 +31,7 @@ let JPushAppKey = "20aeb9ef1cdc4a5185da6bb6"
 let JPushMasterSecret = "71b55a4396de63853ad08c68"
 
 //测试环境外网
-let HOST = "http://182.150.21.104:58092/zbn-web"
+//let HOST = "http://182.150.21.104:58092/zbn-web"
 //测试内网
 //let HOST = "http://172.16.8.52:8081/zbn-web"
 //deng qing
@@ -41,5 +41,5 @@ let HOST = "http://182.150.21.104:58092/zbn-web"
 //liao bing
 //let HOST = "http://172.16.59.47:8081/zbn-web"
 //客户的测试环境
-//let HOST = "http://221.193.233.227:8010/zbn-web"
+let HOST = "http://221.193.233.227:8010/zbn-web"
 
